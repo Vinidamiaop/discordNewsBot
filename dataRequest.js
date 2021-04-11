6bcc6a1b176382e5f31d561e53366d7d61919bb3
Vinicius Damião <vinidamiaop@gmail.com>
2021-04-10 23:49:22 -0300

    I rearranged the code in dataRequest.js

--- dataRequest.js
+++ dataRequest.js
@@ -34,54 +34,62 @@ class DataRequest {
   }
 
   async insertDb() {
-    const data = await this.getData(this.url);
-    console.log("inserting new data");
+    try {
+      const data = await this.getData(this.url);
+      console.log("inserting new data");
 
-    for (let item of data) {
-      let obj = {
-        source: item.source.name,
-        title: item.title,
-        description: item.description,
-        url: item.url,
-        urltoimage: item.urlToImage,
-        publishedat: item.publishedAt,
-        shared: false,
-      };
-      try {
+      for (let item of data) {
+        let obj = {
+          source: item.source.name,
+          title: item.title,
+          description: item.description,
+          url: item.url,
+          urltoimage: item.urlToImage,
+          publishedat: item.publishedAt,
+          shared: false,
+        };
+
         this.db.insertValues(obj);
-      } catch (err) {
-        throw err;
       }
+    } catch (err) {
+      throw err;
     }
   }
 
   // Verify if its new
   async sendNews() {
-    const data = await this.db.getUnsharedNews();
-    return data[data.length - 1];
+    try {
+      const data = await this.db.getUnsharedNews();
+      return data[data.length - 1];
+    } catch (err) {
+      throw err;
+    }
   }
 
   // Embed and send the message to channel
   async embed(channel) {
-    const res = await this.sendNews();
-    if (res) {
-      const embed = new MessageEmbed()
-        .setTitle(res.title)
-        .setURL(res.url)
-        .setColor(0xef271b)
-        .setDescription(res.description)
-        .setImage(res.urltoimage)
-        .setFooter(res.source);
-      channel.send(embed);
-      this.db.updateShared(res.id, true);
-    } else {
-      this.insertDb();
-      console.log(
-        `${new Date().toLocaleDateString("pt-BR", {
-          hour: "2-digit",
-          minute: "2-digit",
-        })} | Nothing New`
-      );
+    try {
+      const res = await this.sendNews();
+      if (res) {
+        const embed = new MessageEmbed()
+          .setTitle(res.title)
+          .setURL(res.url)
+          .setColor(0xef271b)
+          .setDescription(res.description)
+          .setImage(res.urltoimage)
+          .setFooter(res.source);
+        channel.send(embed);
+        this.db.updateShared(res.id, true);
+      } else {
+        console.log(
+          `${new Date().toLocaleDateString("pt-BR", {
+            hour: "2-digit",
+            minute: "2-digit",
+          })} | Nothing New`
+        );
+      }
+    } catch (err) {
+      throw err;
     }
   }
 
